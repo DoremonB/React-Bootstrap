d88e42d90a39c0cd2889048616561420ceb139ad
Rugved <rugved.bongale@somaiya.edu>
2020-03-26 14:10:36 +0530

    Added navbar

--- src/App.js
+++ src/App.js
@@ -4,11 +4,11 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import Home from './components/Home'
-
+import NavbarComponent from './components/NavbarComponents'
 function App() {
   return (
     <div className="App">
-      
+      <NavbarComponent/>
       <Home />
       
     </div>
